66586ef3c0d3231a1071cc6f1a3c0b39cf800368
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-06-28 21:05:47 +0100

    chore(rust): setone challenge 1 setup

--- set-1-basics/rust/src/main.rs
+++ set-1-basics/rust/src/main.rs
@@ -1,13 +1,28 @@
+extern crate base64;
+
+// use base64::{encode, decode};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Set one solutions");
+}
+
+// working with strings is hard in rust ugh
+pub fn hex_to_base64(hex_string: &str) -> String {
+    print!("{}", hex_string);
+    let s = String::new();
+
+    return s
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use super::*;
 
     #[test]
     fn test_hex_to_base64() {
-        assert_eq!(true, true);
+        let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+        let expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+
+        assert_eq!(input, expected);
     }
 }
