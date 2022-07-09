6f58f4ab52e559d4a9215c5c95e396803d7da0f1
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-07-09 14:38:51 +0100

    chore: setup python

--- set-1-basics/python/set_one.py
+++ set-1-basics/python/set_one.py
@@ -1,5 +1,10 @@
-def set_one(hexadecimal):
+# Set One
+
+def hex_to_base64(hexadecimal):
     return hexadecimal
+
+def fixed_xor(buffer_one, buffer_two):
+    return buffer_one + buffer_two
 
 if __name__ == "__main__":
     self.main()
--- set-1-basics/python/set_one_test.py
+++ set-1-basics/python/set_one_test.py
@@ -1,8 +1,19 @@
 import unittest
+from set_one import *
 
 class SetOne(unittest.TestCase):
-    def hex_to_base64(self):
-        self.assertEqual(True, True)
+    def test_hex_to_base64(self):
+        input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+        expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+
+        self.assertEqual(hex_to_base64(input), expected)
+
+    def test_fixed_xor(self):
+        buffer_one = "1c0111001f010100061a024b53535009181c"
+        buffer_two = "686974207468652062756c6c277320657965"
+        xor_combination = "746865206b696420646f6e277420706c6179"
+
+        self.assertEqual(fixed_xor(buffer_one, buffer_two), xor_combination)
 
 if __name__ == "__main__":
     unittest.main()
