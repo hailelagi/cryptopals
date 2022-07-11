9c7b0be8dbce6c199a710819aa025c43f84c8054
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-07-11 20:18:08 +0100

    feat(go + python) hex to base64

--- go/setone/setone.go
+++ go/setone/setone.go
@@ -1,9 +1,27 @@
 package setone
 
+import (
+	"encoding/base64"
+	"encoding/hex"
+)
+
 func HexToBase64(hexStr string) string {
-	return hexStr
+	bin, _ := hex.DecodeString(hexStr)
+	encoded := base64.StdEncoding.EncodeToString([]byte(bin))
+
+	return string(encoded)
 }
 
 func FixedXor(bufferOne, bufferTwo string) string {
-	return "fail"
+	binOne, _ := hex.DecodeString(bufferOne)
+	binTwo, _ := hex.DecodeString(bufferTwo)
+	var fixed = make([]byte, len(binOne))
+
+	for i := range binOne {
+		fixed[i] = binOne[i] ^ binTwo[i]
+	}
+
+	encoded := base64.StdEncoding.EncodeToString([]byte(fixed))
+
+	return string(encoded)
 }
--- python/set_one.py
+++ python/set_one.py
@@ -1,16 +1,12 @@
 # Set One
+import base64
 
-def hex_to_base64(hexadecimal):
-    """
-    Convert hexadecimal to base64
-    """
+def hex_to_base64(hex_str):
+    hex_str = bytes.fromhex(hex_str)
 
-    return hexadecimal
+    return str(base64.b64encode(hex_str))[2:-1]
 
 def fixed_xor(buffer_one, buffer_two):
-    """
-    XOR two buffers
-    """
     return buffer_one + buffer_two
 
 if __name__ == "__main__":
