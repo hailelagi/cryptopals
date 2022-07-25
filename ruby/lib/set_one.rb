d967a454a18d4c0701948a99e885c126b6f2c313
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-07-25 22:38:55 +0100

    feat(ruby): setone hex to base64

--- ruby/lib/set_one.rb
+++ ruby/lib/set_one.rb
@@ -1,6 +1,10 @@
+# require "base64"
+
 class SetOne
-    def hex_to_base64(hex_str)
-        hex_str
+    def hex_to_base64(digest)
+        # tbd: https://stackoverflow.com/questions/9986971/converting-a-hexadecimal-digest-to-base64-in-ruby
+        # what magical sorcery is this
+        [[digest].pack("H*")].pack("m0")
     end
 
     def fixed_xor(buffer_one, buffer_two)
